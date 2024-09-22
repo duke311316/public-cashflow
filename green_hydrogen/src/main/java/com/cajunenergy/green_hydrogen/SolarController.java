e939563a356e890fc6b929eba2ade2095ae72495
Dale Duclion <duke@Dales-MacBook-Pro.local>
2024-09-21 19:00:41 -0500

    added calculate Cost of solar panels

--- green_hydrogen/src/main/java/com/cajunenergy/green_hydrogen/SolarController.java
+++ green_hydrogen/src/main/java/com/cajunenergy/green_hydrogen/SolarController.java
@@ -1,7 +1,10 @@
 package com.cajunenergy.green_hydrogen;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +12,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class SolarController {
     @Autowired
     private SolarService solarService;
+
+    @PostMapping("/calculate_cost_per_kilogram")
+    public Double calculateCostPerKilo(
+        @RequestParam Double costPerWattSolar, 
+        @RequestParam Double solarEfficiency, 
+        @RequestParam Integer costPerKilowattElectrolyzer, 
+        @RequestParam String typeOfElectrolyzer,
+        @RequestParam Integer goalKilogramsPerYear,
+        @RequestParam String countryOfOrigin){
+
+            Integer systemDailyWattage = solarService.calculateSolarWattage(goalKilogramsPerYear);
+            Double solarPanelCost = solarService.calculateSolarPanelTotalPrice(costPerWattSolar, systemDailyWattage);
+            Double taxAndTariff = solarService.addTaxAndTariff(countryOfOrigin);
+            Double panelCostIncludingTaxAndtariff = (1 + taxAndTariff) * solarPanelCost;
+
+            return panelCostIncludingTaxAndtariff;
+
+
+    }
+    
 
     
 
